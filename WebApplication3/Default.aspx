88bd2094d3793d6934cf1735dc7766ee9129df8a
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2018-09-21 17:44:35 -0500

    added project

---
+++ WebApplication3/Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication3._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+	<title>Untitled Page</title>
+	<style>
+		td input[name^=ImageButton]
+		{
+			width:25px;	
+		}
+		table, td
+		{
+			border: 1px solid black;
+		}
+	</style>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+		<asp:Table runat="server" ID="tbl"></asp:Table>
+		<table>
+			<tr>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton1" ImageUrl="one.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton2" ImageUrl="two.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton3" ImageUrl="three.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton4" ImageUrl="four.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton5" ImageUrl="five.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton6" ImageUrl="six.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton7" ImageUrl="seven.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton8" ImageUrl="eight.png" />
+				</td>
+				<td>
+					<asp:ImageButton runat="server" id="ImageButton9" ImageUrl="nine.png" />
+				</td>
+			</tr>
+		</table>
+	</div>
+	</form>
+</body>
+</html>
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WebApplication3
+{
+	public partial class _Default : System.Web.UI.Page
+	{
+		public _Default()
+		{
+			Rows = 9;
+			Columns = 9;
+			DefaultImage = "zero.png";
+		}
+		protected void Page_Init(object sender, EventArgs e)
+		{
+			img = new ImageButton[Rows][];
+			for (int i = 0; i < img.Length; i++) {
+				img[i] = new ImageButton[Columns];
+				tbl.Rows.Add(new TableRow());
+				for (int j = 0; j < img[i].Length; j++) {
+					img[i][j] = new ImageButton();
+					img[i][j].Width = new Unit(25);
+					if (!IsPostBack) {
+						img[i][j].ImageUrl = DefaultImage;
+					}
+					tbl.Rows[i].Cells.Add(new TableCell());
+					tbl.Rows[i].Cells[j].Controls.Add(img[i][j]);
+				}
+			}
+		}
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			for (int i = 0; i < img.Length; i++) {
+				for (int j = 0; j < img[i].Length; j++) {
+					if (!IsPostBack) {
+						img[i][j].ImageUrl = DefaultImage;
+					}
+					img[i][j].Click += new ImageClickEventHandler(_Default_Click);
+				}
+			}
+		}
+
+		void _Default_Click(object sender, ImageClickEventArgs e)
+		{
+			((ImageButton)sender).ImageUrl = "one.png";
+			((TableCell)((ImageButton)sender).Parent).BorderColor = Color.Red;
+		}
+		public string DefaultImage { get; set; }
+		public int Rows { get; set; }
+		public int Columns { get; set; }
+		public ImageButton[][] img;
+		public string[][] PathInfo;
+	}
+	public class CPathInfo
+	{
+		public CPathInfo()
+		{ 
+		}
+	}
+}
